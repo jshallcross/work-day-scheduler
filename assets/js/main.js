786d21220647c364f5d6eb5e2a63411b7dac6171
Jon Shallcross <jshallcross1@icloud.com>
2021-01-14 13:34:52 -0500

    all inputs save to local storage upon click event

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,20 +5,11 @@ var saveBtn = $(".saveBtn");
 $("#currentDay").text(moment().format('dddd MMMM Do, YYYY'));
 
 
-/*
-saveBtn.on("click", function (){
-    event.preventDefault();
-    var hourId = $(this).attr("id").split("-")[1];
-    var userInput = $("#hour-" + hourId).val();
-    console.log(hourId, userInput);
-    //localStorage.setItem(hourId, JSON.stringify(userInput));
-})
-*/
 
 saveBtn.on("click", function (){
     event.preventDefault();
     var hourId = $(this).attr("id");
-    var userInput = $("#9").val();
+    var userInput = $("#save"+hourId).val();
     console.log(hourId, userInput);
-    localStorage.setItem(hourId, JSON.stringify(userInput));
+    //localStorage.setItem(hourId, JSON.stringify(userInput));
 })
