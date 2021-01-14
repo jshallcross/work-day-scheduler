7b2d77d65329f877fda11531f134c3b6d829e0d4
Jon Shallcross <jshallcross1@icloud.com>
2021-01-14 12:09:23 -0500

    First hour saves to local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
 
+var saveBtn = $(".saveBtn");
+
 // display current date
 $("#currentDay").text(moment().format('dddd MMMM Do, YYYY'));
+
+
+/*
+saveBtn.on("click", function (){
+    event.preventDefault();
+    var hourId = $(this).attr("id").split("-")[1];
+    var userInput = $("#hour-" + hourId).val();
+    console.log(hourId, userInput);
+    //localStorage.setItem(hourId, JSON.stringify(userInput));
+})
+*/
+
+saveBtn.on("click", function (){
+    event.preventDefault();
+    var hourId = $(this).attr("id");
+    var userInput = $("#9").val();
+    console.log(hourId, userInput);
+    localStorage.setItem(hourId, JSON.stringify(userInput));
+})
