cdfeab7df541cc04337fc3edd87f693054807ba8
Jon Shallcross <jshallcross1@icloud.com>
2021-01-14 14:47:56 -0500

    added time check to change colour based on current time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,56 @@
 
 var saveBtn = $(".saveBtn");
 
+
+
+
+
+
 // display current date
 $("#currentDay").text(moment().format('dddd MMMM Do, YYYY'));
 
 
+function onLoad() {
+    
+}
 
-saveBtn.on("click", function (){
-    event.preventDefault();
-    var hourId = $(this).attr("id");
-    var userInput = $("#save"+hourId).val();
-    console.log(hourId, userInput);
-    //localStorage.setItem(hourId, JSON.stringify(userInput));
-})
+
+
+
+$(document).ready(function(){
+    timeCheck();
+
+    saveBtn.on("click", function (){
+        event.preventDefault();
+        var hourId = $(this).attr("id");
+        var userInput = $("#save"+hourId).val();
+        console.log(hourId, userInput);
+        localStorage.setItem(hourId, JSON.stringify(userInput));
+    });
+    
+
+
+});
+
+
+function timeCheck() {
+    var currentHour = moment().hour();
+    console.log(currentHour);
+    $('.input-form').each(function (){
+        var hour = $(this).attr("id").split("save")[1];
+        if (hour < currentHour) {
+            $(this).addClass('past');
+        } else if (hour == currentHour) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+
+
+
+
