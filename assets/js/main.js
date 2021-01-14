961ee3a15a43c0a6d699de95e515b218da7af710
Jon Shallcross <jshallcross1@icloud.com>
2021-01-14 16:33:38 -0500

    made app mobile friendly

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,52 +5,48 @@ var saveBtn = $(".saveBtn");
 
 
 
-
 // display current date
 $("#currentDay").text(moment().format('dddd MMMM Do, YYYY'));
-
-
-function onLoad() {
-    
-}
 
 
 
 
 $(document).ready(function(){
+
     timeCheck();
 
     saveBtn.on("click", function (){
         event.preventDefault();
+        console.log(this);
         var hourId = $(this).attr("id");
         var userInput = $("#save"+hourId).val();
         console.log(hourId, userInput);
-        localStorage.setItem(hourId, JSON.stringify(userInput));
+        
+        localStorage.setItem(hourId,userInput);   
     });
-    
-
-
 });
 
-
+//Checks current time and compare it to the hour on the time blocks
 function timeCheck() {
     var currentHour = moment().hour();
-    console.log(currentHour);
+    
     $('.input-form').each(function (){
         var hour = $(this).attr("id").split("save")[1];
         if (hour < currentHour) {
             $(this).addClass('past');
         } else if (hour == currentHour) {
-            $(this).removeClass('past');
             $(this).addClass('present');
         } else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
             $(this).addClass("future");
         }
     });
 }
 
 
+setInterval(function(){
+    $('.input-form').each(function(){
+        timeCheck($(this));
+    });
+}, 1800000);
 
 
